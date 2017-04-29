8cc3be5f45ee1533954cec00aa9b6e3e21cf1a1d
Antonio Policastro <tonypolik@gmail.com>
2017-04-29 15:16:23 +0200

    Simple vue app

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Hello worlds
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
