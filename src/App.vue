51d112488d4c338b1979c8e6c8d230f5d8a2732b
Antonio Policastro <tonypolik@gmail.com>
2017-04-29 16:07:58 +0200

    Vue: template "webpack" instead of "webpack-simple"

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-    Hello worlds
-    <h1>{{msg}}</h1>
+    <hello></hello>
   </div>
 </template>
 
 <script>
+import Hello from './components/Hello'
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  components: {
+    Hello
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
